b20300375a85e3f215d8e0ff49fbf35678b16f76
Mirza Muhammad Abbas <abbasmeerza786@gmail.com>
2022-10-12 19:39:35 +0500

    Added MyThread

--- TypingTutorDemo.java
+++ TypingTutorDemo.java
@@ -1,3 +1,21 @@
+class MyThread extends Thread{      
+
+    int errors;
+    double accuracy, speed;
+
+    MyThread(int err, double acc, double sp){     
+
+        errors=err;
+        accuracy=acc;
+        speed=sp;
+    }
+    public void run(){
+
+        JOptionPane P=new JOptionPane();
+        P.showMessageDialog(null,("Errors: " + errors + "\nAccuracy: " + new DecimalFormat("#0.00").format(accuracy) + "%" + "\nSpeed: " + new DecimalFormat("#00.00").format(speed) + " wpm"));
+
+    }
+}
 
 class TypingTutor{
 
