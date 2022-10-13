09075a15135309b75ec7e5aa592014ade21c8d24
Mirza Muhammad Abbas <abbasmeerza786@gmail.com>
2022-10-13 19:21:05 +0500

    Improved GUI

--- TypingTutorDemo.java
+++ TypingTutorDemo.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import java.text.DecimalFormat;
+import java.lang.*;
+
 class MyThread extends Thread{      
 
     int errors;
@@ -19,6 +26,24 @@ class MyThread extends Thread{
 
 class TypingTutor{
 
+    private final int WIDTH = 610, LENGTH = 650;    
+
+    private JTextArea promptBox, typeBox;
+    private JButton ResetButton;
+    private JLabel Name, wrongLabel, numErrorsLabel, accuracyLabel, wpmLabel;
+    private ArrayList <JButton> buttons;
+    private String qwerty, typedText, Text;
+    private double accuracy, startTime, endTime, speed;
+    private int currentChar, rowBreak1, rowBreak2, numErrors, numKeysTyped;
+
+    JPanel P1=new JPanel();
+    JLabel L1=new JLabel();
+
+    TypingTutor(){
+
+    }
+
+    
 }
 
 class TypingTutorDemo{
